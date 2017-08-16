7099fd0b1612d5f6e4456181ad7776c670630250
Ben Hartshorne <ben@honeycomb.io>
2017-08-15 23:29:58 -0700

    moar

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type Options struct {
 	Password  string `short:"p" long:"password" descrption:"Password to authenticate with the Plum Web API"`
 	StateFile string `short:"f" long:"file" description:"location to store state file" default:"/var/lib/plumd.state"`
 	Debug     bool   `short:"d" long:"debug" description:"enable debugging output"`
+	Test      bool   `short:"t" long:"test" description:"run the app with mocked plum"`
 }
 
 func main() {
@@ -28,30 +29,55 @@ func main() {
 		logrus.Debug("Running in debug mode")
 	}
 
-	house := libplum.PlumHouse{}
+	house := libplum.NewHouse()
+	if options.Test {
+		logrus.Debug("Running in test mode")
+		house = libplum.NewTestHouse()
+	}
 
 	// if we have a state file, load it
 	if file, err := os.Open(options.StateFile); err == nil {
 		contents, err := ioutil.ReadAll(file)
 		if err != nil {
-			panic(err)
+			logrus.WithField("error", err).Error("failed to read state file")
 		}
 		house.LoadState(contents)
+		// spew.Dump(house)
+		logrus.WithField("house", house).Debug("loaded house config from state file")
+		// and spin up a goroutine to save it in the background periodically
+		go func() {
+			tick := time.NewTicker(30 * time.Second).C
+			for {
+				select {
+				case <-tick:
+					contents, err = house.SaveState()
+					if err != nil {
+						logrus.WithField("error", err).Error("failed to get serialized house state")
+					}
+					err = ioutil.WriteFile(options.StateFile, contents, 0644)
+					if err != nil {
+						logrus.WithField("error", err).Error("failed to write state file")
+					}
+				}
+			}
+		}()
 	}
 
-	// update email and password from flags
-	if options.Email != "" {
-		house.Email = options.Email
-	}
-	if options.Password != "" {
-		house.Password = options.Password
+	if options.Email != "" && options.Password != "" {
+		house.SetCreds(&libplum.Account{
+			Email:    options.Email,
+			Password: options.Password,
+		})
 	}
 
-	err := house.Initialize("")
+	err := house.Initialize()
 	if err != nil {
 		panic(err)
 	}
-
+	time.Sleep(5 * time.Second)
+	house.Update()
+	time.Sleep(5 * time.Second)
+	// spew.Dump(house)
 	nook, err := house.GetLoadByName("Nook")
 	if err, ok := err.(libplum.ENotFound); ok {
 		fmt.Println(err)
